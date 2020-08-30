6614e0e209dc49bcc8f68284aae7f162b7ae7353
johyeongjun <compa513@naver.com>
2020-08-30 14:41:28 +0900

    don't need

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/hoho', function(req, res, next) {
-  res.send('respond with a hoho');
+  res.send('respond with a hohohohohohohoh');
 });
 
 router.get('/hohoho', function(req, res, next) {
-  res.json({name: 'jojo', age: 26});
+  res.json({name: 'jojojojojjo', age: 26});
 });
 module.exports = router;
