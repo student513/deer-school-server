d2324328314c06d7ae1b72e6817148b0e0c3f81e
johyeongjun <compa513@naver.com>
2020-08-30 15:12:04 +0900

    add login

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('login with a resource');
 });
 
 router.get('/hoho', function(req, res, next) {
